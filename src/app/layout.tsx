1ee3b824b5eeae0336e39d8833d6b9d2266fc954
ionkarsingh <onkar.fcs@gmail.com>
2026-03-21 00:58:51 +0530

    Layout bug Fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-  display: "swap",
-  preload: false,
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-  display: "swap",
-  preload: false,
-});
 
 export const metadata: Metadata = {
   title: "Magic Brush Admin",
@@ -37,9 +22,7 @@ export default function RootLayout({
           referrerPolicy="no-referrer"
         />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         {children}
       </body>
     </html>
